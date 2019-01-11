12bcc91076d68f36b0819a7e03f8405dda293d88
Joan in DC <joanrigdon@gmail.com>
2019-01-11 14:44:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,10 +33,12 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
       winning_team<< "X"
+      winning_team
     elsif 
       position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
       winning_team<< "0"
+      winning_team
     else
       false
     end
